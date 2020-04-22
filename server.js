756f14994e15a5530f14d661e989d88604dbece8
Nikhil N G <62535502+nikhilng99@users.noreply.github.com>
2020-04-23 00:22:13 +0530

    change

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors')
 const knex = require('knex')
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const register = require('./controllers/register')
 const signin = require('./controllers/signin')
 const profile = require('./controllers/profile')
 const image = require('./controllers/image')
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const db = knex({
     client: 'pg',
     connectionString: {
-        host: process.env.DATABASE_URL,
+        host: 'postgresql-animated-83315',
        ssl:true
     }
 });
@@ -32,5 +32,5 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`app running on port ${process.env.PORT}`)
+    console.log('app running on port ')
 })
