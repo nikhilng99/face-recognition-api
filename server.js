9ac132fe1486acbbec7a6495614b71303f154561
Nikhil N G <62535502+nikhilng99@users.noreply.github.com>
2020-04-23 00:46:13 +0530

    changes fix

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log(`app running on port ${process.env.PORT}`)
 })
