0cd702bebb2478afd20eba0b57cd98dad89b3617
Nikhil N G <62535502+nikhilng99@users.noreply.github.com>
2020-04-23 00:42:59 +0530

    change

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 app.listen(process.env.PORT || 3000, () => {
-    console.log('app running on port ')
+    console.log(`app running on port ${process.env.PORT}`)
 })
